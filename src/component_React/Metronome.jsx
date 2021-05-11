3fa923cf77cfac9c777a7794af98435ac4bdaa69
Targarius <theshark158@gmail.com>
2021-05-11 13:42:31 +0200

    audioctx issue

--- src/component_React/Metronome.jsx
+++ src/component_React/Metronome.jsx
@@ -1,13 +1,19 @@
 import React, {useState, useEffect} from 'react';
-import MetronomeController from './Metronome_Controller'
-import MetronomeInfo from './Metronome_Info'
-
+import MetronomeController from './Metronome_Controller';
+import MetronomeInfo from './Metronome_Info';
+import {Howl} from 'howler';
 
 const Metronome = () => {
     const [tempo, setTempo] = useState(100);
     const [temps, setTemps] = useState(0);
     const [mesure, setMesure] = useState(0);
     const [timerUp, setTimerUp] = useState(false);
+
+    const sound = new Howl({
+        src: ['../audio/metronomeSound.mp3'],
+        html5: true
+    });
+    sound.play(); 
 
     const toogleTimer = () =>{
         setTimerUp(!timerUp);
@@ -21,8 +27,8 @@ const Metronome = () => {
 
     useEffect(() => {
         let timer = null;
-
         if (timerUp) {
+            const audioCtx = new AudioContext();
             timer = setInterval(() => {
                 if (mesure < 4) {
                     setMesure( m => m + 1)
@@ -30,6 +36,9 @@ const Metronome = () => {
                     setMesure( m => 1);
                     setTemps( t => t + 1);
                 } 
+            
+            sound.play();
+            audioCtx.resume();
             }, Math.round(60000 / tempo));
         } else if (!timerUp) {
             clearInterval(timer);
